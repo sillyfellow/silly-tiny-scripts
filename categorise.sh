38fb6d0e3ea40f9e040c20db3da719f945ddce15
Sandeep Sadanandan <sandeep.sadanandan@mapcase.de>
2015-04-12 13:28:10 +0200

    fix typo

--- categorise.sh
+++ categorise.sh
@@ -27,8 +27,8 @@ cat ??.txt > $XTEMP
 TEMPFILE=$(mktemp)
 grep -v -f $XTEMP allsongs.txt | shuf > ${TEMPFILE}
 
-echo "`wc -l ${XTEMP} songs are done`"
-echo "`wc -l ${TEMPFILE} more to go`"
+echo "`wc -l ${XTEMP}` songs are done"
+echo "`wc -l ${TEMPFILE}` more to go"
 
 while read song 
 do
